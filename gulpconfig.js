a0f85bded47e0e03885d77f289a2f2f33d3512d1
Ruven Pelka <ruven.pelka@gmail.com>
2016-05-30 05:59:27 -0400

    Open bowser via browsersync when gulp is started

--- gulpconfig.js
+++ gulpconfig.js
@@ -15,7 +15,7 @@ module.exports = {
   browsersync: {
     files: [build+'/**', '!'+build+'/**.map'] // Exclude map files
   , notify: true // In-line notifications (the blocks of text saying whether you are connected to the BrowserSync server or not)
-  , open: false // Set to false if you don't like the browser window opening automatically
+  , open: true // Set to false if you don't like the browser window opening automatically
   , port: 3000 // Port number for the live version of the site; default: 3000
   , proxy: 'localhost:8888/_wp/demo-content' // We need to use a proxy instead of the built-in server because WordPress has to do some server-side rendering for the theme to work
   , watchOptions: {
